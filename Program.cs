bb06eb083a07663399a31974310c8d5a9ae7f0ec
Gino Joseph <gino@limabean.co.za>
2018-06-09 13:38:11 +0200

    Basic Number guesser game. TODO: refactor

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+// Namespaces
+namespace NumberGuesser
+{
+    // Main Class
+    class Program
+    {
+        // Entry Point Method
+        // static refers to function itself no need to instantiate
+        // void -> i.e. no returned data
+        static void Main(string[] args)
+        {
+            // app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Gino Joseph";
+
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Green;
+            // Write app info to console
+            Console.WriteLine("{0}: Version {1} By {2}", appName, appVersion, appAuthor);
+            Console.ResetColor();
+
+            // Ask user name
+            Console.WriteLine("What is your name?");
+            // Get user input
+            string inputName = Console.ReadLine();
+            // Write user name
+            Console.WriteLine("Hello {0}, let's play a game.", inputName);
+
+            // Set correct number
+            int correctNumber = 7;
+
+            // init guess var
+            int guess = 0;
+
+            Console.WriteLine("Guess a number between 1 and 10");
+            while(guess != correctNumber)
+            {
+                // Get user guess input
+                string input = Console.ReadLine();
+                // cast to int and put into guess
+                guess = Int32.Parse(input);
+
+                // match guess to correct number
+                if(guess != correctNumber)
+                {
+                    // Change text color error
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    // Write error msg
+                    Console.WriteLine("Wrong Number, please try again");
+                    Console.ResetColor();
+                }
+            }
+
+            // output success message
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            // Write error msg
+            Console.WriteLine("You are CORRECT");
+            Console.ResetColor();
+        }
+    }
+}
